8b0d5acab36ff2c29aa4c04845dacfab369d685b
Maria Luiza Lima <luizagamesl90@gmail.com>
2026-01-14 16:57:08 -0300

    Files, index, about, home ...

---
+++ script.js
@@ -0,0 +1,28 @@
+const livro = document.querySelector('.livro');
+const img = document.querySelector('.livro-fechado');
+const secaoProjetos = document.querySelector('#projects'); 
+livro.addEventListener('click', () => {
+    if (!livro.classList.contains('estagio-1') && !livro.classList.contains('estagio-2')) {
+        // ESTADO: Estava fechado -> PAGE 1
+        livro.classList.add('estagio-1');
+        img.src = 'img/page-1.png'; 
+        
+    } else if (livro.classList.contains('estagio-1')) {
+        // ESTADO: Estava na Página 1 ->PAGE 2
+        livro.classList.remove('estagio-1');
+        livro.classList.add('estagio-2');
+        img.src = 'img/page-2.png'; 
+        
+    } else {
+        // ESTADO: Estava na Página 2 -> Fecha novamente
+        livro.classList.remove('estagio-2');
+        img.src = 'img/livro-fechado.png'; 
+
+        setTimeout(() => {
+            secaoProjetos.scrollIntoView({ 
+                behavior: 'smooth', 
+                block: 'start'      
+            });
+        }, 400); 
+    }
+});
